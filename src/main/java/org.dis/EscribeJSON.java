3c1a02838e7aca20ccd27c991e2c0f62d4846081
Enrique Ruiz <7836092@alumnos.ufv.es>
2020-11-19 21:31:16 +0100

    commit con cosas mal

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -3,10 +3,37 @@ package org.dis;
 
 //imports ...
 
+import com.google.gson.Gson;
+import com.sun.xml.internal.ws.server.ServerRtException;
+
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EscribeJSON {
 
     public static void main(String[] args) {
+        LeerCSV.leer_csv("best-golf-players.csv");
+        try {
+            // create a map
+            Map<String, Object> map = new HashMap<>();
 
+            map.put();
+
+
+            // create a writer
+            Writer writer = new FileWriter("players.json");
+
+            // convert map to JSON File
+            new Gson().toJson(map, writer);
+
+            // close the writer
+            writer.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
     
  }
--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -11,31 +11,38 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 // Otros imports ...
 
 public class LeerCSV {
 
-    public List<POJO> parseaCSV(String path) {
-        ArrayList<POJO> players = CsvToBean.parse();
+    public static void leer_csv(String path) {
+        List<POJO> players = CsvToBean.parse();
+        POJO p = new POJO();
 
         try(
-                Reader reader = Files.newBufferedReader(Paths.get("best-golf-player.csv"));
+                Reader reader = Files.newBufferedReader(Paths.get(path);
                 CSVReader csvReader = new CSVReader(reader);
                 ){
 
             String[] nextData;
-            while((nextData = csvReader.readNext()) != null){
 
-                players.add()
+            while ((nextData = csvReader.readNext()) != null) {
+
+                p.setPosicion(nextData[0]);
+                p.setNombre(nextData[1]);
+                p.setApellido(nextData[2]);
+                p.setPais(nextData[3]);
+                p.setEdad(nextData[4]);
+
+                players.add(p);
             }
 
         } catch (IOException | CsvValidationException e) {
             e.printStackTrace();
         }
 
-
-        return players;
     }
 }
